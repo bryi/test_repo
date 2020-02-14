1c6ce4e5db5e6d983cae903a65d90aaac5c84bb6
bryi <a.sazonov@ctm.ru>
2020-02-14 12:30:24 +0000

    python api

---
+++ src/models/__init__.py
@@ -0,0 +1,9 @@
+#src/models/__init__.py
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
+#from .BlogpostModel import BlogpostModel, BlogpostSchema
+#from .UserModel import UserModel, UserSchema
+
+# initialize our db
+db = SQLAlchemy()
+bcrypt = Bcrypt()
