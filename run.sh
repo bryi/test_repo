3c8ef0fb315cdd4af6fcc206eb50f9279b2e02a2
bryi <a.sazonov@ctm.ru>
2020-02-18 14:55:39 +0000

    changed run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,7 @@ export FLASK_ENV=development
 export DATABASE_URL=postgres://postgres:1@127.0.0.1:5432/blog_api_db
 export JWT_SECRET_KEY=$(python3 -c 'import os; print(os.urandom(24))')
 #echo $JWT_SECRET_KEY
+python3 manage.py db init
+python3 manage.py db migrate
+python3 manage.py db upgrade
 python3 run.py
