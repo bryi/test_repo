328367961e9d0bd2d75ed1b66f2d7a7b43b2d11f
bryi <a.sazonov@ctm.ru>
2020-02-19 14:49:55 +0000

    add unit test

--- src/config.py
+++ src/config.py
@@ -8,6 +8,15 @@ class Development(object):
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 
+class Testing(object):
+    """
+    Development environment configuration
+    """
+    TESTING = True
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+
 class Production(object):
     """
     Production environment configurations
@@ -20,4 +29,5 @@ class Production(object):
 app_config = {
     'development': Development,
     'production': Production,
+    'testing': Testing,
 }
