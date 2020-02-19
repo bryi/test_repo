1fd6fa5dfed29ada7e0bc65cb14ce652ddae3af4
bryi <a.sazonov@ctm.ru>
2020-02-19 10:05:37 +0000

    change script

--- run.sh
+++ run.sh
@@ -1,11 +1,21 @@
+#CLEAR DATABASE
 psql -U postgres -d blog_api_db -c "DROP TABLE alembic_version"
 psql -U postgres -d blog_api_db -c "DROP TABLE blogposts"
 psql -U postgres -d blog_api_db -c "DROP TABLE users"
+
+#MAKE ENVIRONMENTS
 export FLASK_ENV=development
 export DATABASE_URL=postgres://postgres:1@127.0.0.1:5432/blog_api_db
 export JWT_SECRET_KEY=$(python3 -c 'import os; print(os.urandom(24))')
-#echo $JWT_SECRET_KEY
+export PYTHONPATH=$(pwd)
+
+#MAKE_DATABASE
 python3 manage.py db init
 python3 manage.py db migrate
 python3 manage.py db upgrade
+
+#MAKE_LINT_TESTS
+pylint run.py
+
+#GO
 python3 run.py
