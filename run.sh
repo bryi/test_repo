6a1a20c822eb7b88446d23955861db0589fccc73
bryi <a.sazonov@ctm.ru>
2020-02-19 07:27:07 +0000

    changed script run

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
+psql -U postgres -d blog_api_db -c "DROP TABLE alembic_version"
+psql -U postgres -d blog_api_db -c "DROP TABLE blogposts"
+psql -U postgres -d blog_api_db -c "DROP TABLE users"
 export FLASK_ENV=development
 export DATABASE_URL=postgres://postgres:1@127.0.0.1:5432/blog_api_db
 export JWT_SECRET_KEY=$(python3 -c 'import os; print(os.urandom(24))')
