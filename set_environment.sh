b6877e4c7b921c00c969e45b5f9b51fd89151481
bryi <a.sazonov@ctm.ru>
2020-02-20 08:35:55 +0000

    added environment script

---
+++ set_environment.sh
@@ -0,0 +1,4 @@
+export FLASK_ENV=development
+export DATABASE_URL=postgres://postgres:1@127.0.0.1:5432/blog_api_db
+export JWT_SECRET_KEY=$(python3 -c 'import os; print(os.urandom(24))')
+export PYTHONPATH=$(pwd)
