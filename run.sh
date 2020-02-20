bbafc41e1308fa97e0d1dc4479a998d633b1a388
bryi <a.sazonov@ctm.ru>
2020-02-20 10:30:48 +0000

    changed script

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 sudo rm -rf migrations/
-sudo chown bryi $(pwd)
+sudo chown $USER $(pwd)
 #CLEAR DATABASE
 psql -U postgres -d blog_api_db -c "DROP TABLE alembic_version"
 psql -U postgres -d blog_api_db -c "DROP TABLE blogposts"
 psql -U postgres -d blog_api_db -c "DROP TABLE users"
 
 #MAKE ENVIRONMENTS
-export FLASK_ENV=development
-export DATABASE_URL=postgres://postgres:1@127.0.0.1:5432/blog_api_db
-export JWT_SECRET_KEY=$(python3 -c 'import os; print(os.urandom(24))')
-export PYTHONPATH=$(pwd)
+#export FLASK_ENV=development
+#export DATABASE_URL=postgres://postgres:1@127.0.0.1:5432/blog_api_db
+#export JWT_SECRET_KEY=$(python3 -c 'import os; print(os.urandom(24))')
+#export PYTHONPATH=$(pwd)
 
 #MAKE_DATABASE
 python3 manage.py db init
